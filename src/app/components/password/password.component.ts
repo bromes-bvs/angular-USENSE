bfaa8be30796c4dc1f4b39d29c32c6f58e35b6c6
Borys Supreme <bvs.bromes@gmail.com>
2023-07-23 05:13:04 +0300

    add tailwind view

---
+++ src/app/components/password/password.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-password',
+  templateUrl: './password.component.html',
+  styleUrls: ['./password.component.scss'],
+})
+export class PasswordComponent {
+  strength: string = 'low';
+  errorType: string = 'letter';
+}
